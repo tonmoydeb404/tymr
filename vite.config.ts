e354e498e5b7f125602c211cc937a558df7810db
tonmoydeb404 <tonmoydeb404@gmail.com>
2025-01-13 10:11:12 +0600

    feat: add PWA support with service worker registration and manifest file

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     port: 4030,
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({ registerType: "autoUpdate", devOptions: { enabled: true } }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
